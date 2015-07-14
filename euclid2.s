0251ff77410e3fddadb4ac436e9ee024af9c4a3f
dramenti <empoleon5000@yahoo.com>
2015-07-14 08:26:27 +0000

    Add comments

--- euclid2.s
+++ euclid2.s
@@ -1,18 +1,18 @@
 .code
-jump :main
-_gcd setr r3 0
-eq r3 r2
-retcon r3
-movr r4 r1
+jump :main ;begin program
+_gcd setr r3 0 ;set register 3 to 0
+eq r3 r2 ; check if b == 0
+retcon r3 ; if b == 0 then return a (register 1)
+movr r4 r1 ; otherwise do a, b = b, a%b
 movr r1 r2
 imod r4 r2
 movr r2 r4
-jump :gcd
-_main syscall readint
-movr r1 sys1
+jump :gcd ; call gcd again, use jump as tail-recursion to save memory
+_main syscall readint ; read user input into sys1 register
+movr r1 sys1 ; move user input into r1, repeat
 syscall readint
 movr r2 sys1
-call :gcd
-movr sys0 r1
-syscall printint
-exit
+call :gcd ; call gcd with args r1, r2
+movr sys0 r1 ; move value to print into sys0 register
+syscall printint ; prints content of sys0 register
+exit ; done
